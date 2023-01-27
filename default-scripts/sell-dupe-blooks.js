372f5dcfaf3a0873c3657f4cdb87628f50ee0d11
soft-shelled-turtle <122046809+soft-shelled-turtle@users.noreply.github.com>
2023-01-27 18:01:22 +0000

    .

--- default-scripts/sell-dupe-blooks.js
+++ default-scripts/sell-dupe-blooks.js
@@ -1,85 +1,46 @@
-(async () => {
-    let n = document.createElement('iframe');
-    document.body.append(n);
-    window.alert = n.contentWindow.alert.bind(window);
-    window.prompt = n.contentWindow.prompt.bind(window);
-    window.confirm = n.contentWindow.confirm.bind(window);
-    n.remove();
+async function getName(authToken) {
+    const response = await fetch('https://api.blooket.com/api/users/verify-token?token=JWT+' + authToken);
+    const data = await response.json();
 
-    var getValues = () => new Promise((e, t) => {
-        try {
-            let n = window.webpackJsonp.map(e => Object.keys(e[1]).map(t => e[1][t])).reduce((e, t) => [...e, ...t], []).find(e => /\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/.test(e.toString()) && /\(new TextEncoder\)\.encode\(\"(.+?)\"\)/.test(e.toString())).toString();
-            e({
-                blooketBuild: n.match(/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/)[0],
-                secret: n.match(/\(new TextEncoder\)\.encode\(\"(.+?)\"\)/)[1]
-            })
-        } catch {
-            t("Could not fetch auth details")
-        }
-    })
-    var encodeValues = async (e, t) => {
-        let d = window.crypto.getRandomValues(new Uint8Array(12));
-        return window.btoa(Array.from(d).map(e => String.fromCharCode(e)).join("") + Array.from(new Uint8Array(await window.crypto.subtle.encrypt({
-            name: "AES-GCM",
-            iv: d
-        }, await window.crypto.subtle.importKey("raw", await window.crypto.subtle.digest("SHA-256", (new TextEncoder).encode(t)), {
-            name: "AES-GCM"
-        }, !1, ["encrypt"]), (new TextEncoder).encode(JSON.stringify(e))))).map(e => String.fromCharCode(e)).join(""))
-    };
-            fetch("https://api.blooket.com/api/users", { credentials: "include" }).then(x => x.json()).then(x => {
-                let blooks = Object.entries(x.unlocks).map(x => [x[0], x[1] - 1]).filter(x => x[1] > 0);
-                let wait = ms => new Promise(r => setTimeout(r, ms));
-                getValues().then(async e => {
-                    let error = false;
-                    alert('Selling duplicate blooks, please wait');
-                    for (let [blook, numSold] of blooks) {
-                        fetch("https://api.blooket.com/api/users/sellblook", {
-                            method: "put",
-                            credentials: "include",
-                            headers: {
-                                "content-type": "application/json",
-                                "X-Blooket-Build": e.blooketBuild
-                            },
-                            body: await encodeValues({
-                                name: x.name,
-                                blook,
-                                numSold
-                            }, e.secret)
-                        }).catch(() => { error = true });
-                        await wait(750);
-                        if (error) break;
-                    }
-                    alert(`Results:\n` + blooks.map((x) => `    ${x[1]} ${x[0]}`).join(`\n`));
-                }).catch(() => alert('There was an error encoding requests!'));
-            }).catch(() => alert('There was an error getting user data!'));
-})();
-
-
-function footer() {
-    let element = document.createElement('div');
-
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    element.onmousedown = ((e = window.event) => {
-        e.preventDefault();
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = (() => {
-            document.onmouseup = null;
-            document.onmousemove = null;
-        });
-        document.onmousemove = ((e) => {
-            e = e || window.event;
-            e.preventDefault();
-            pos1 = pos3 - e.clientX;
-            pos2 = pos4 - e.clientY;
-            pos3 = e.clientX;
-            pos4 = e.clientY;
-            let top = (element.offsetTop - pos2) > 0 ? (element.offsetTop - pos2) : 0;
-            let left = (element.offsetLeft - pos1) > 0 ? (element.offsetLeft - pos1) : 0;
-            element.style.top = top + "px";
-            element.style.left = left + "px";
-        });
-    });
+    return data.name
 };
 
-footer();
+async function getBlooks(blooketName) {
+    const response = await fetch('https://api.blooket.com/api/users/blooks?name=' + blooketName, {
+        headers: {
+            "referer": "https://www.blooket.com/",
+            "content-type": "application/json",
+            "authorization": localStorage.token
+        }
+    });
+    const data = await response.json();
+
+    return data
+};
+
+async function sellDupeBlooks() {
+    const myToken = localStorage.token.split('JWT ')[1];
+    const blooketName = await getName(myToken);
+    const blooks = await getBlooks(blooketName);
+    const x = Object.entries(blooks).filter(i => i[1] > 1).map(i => [i[0], i[1] - 1])
+
+    for (const [blook, val] of x) {
+        await fetch('https://api.blooket.com/api/users/sellblook', {
+            method: "PUT",
+            headers: {
+                "referer": "https://www.blooket.com/",
+                "content-type": "application/json",
+                "authorization": localStorage.token
+            },
+            body: JSON.stringify({
+                blook: blook,
+                name: blooketName,
+                numSold: val
+            })
+        });
+    };
+
+    alert('Results:\n' + x.map(x => `    ${x[1]} ${x[0]}`).join('\n'))
+};
+
+sellDupeBlooks();
