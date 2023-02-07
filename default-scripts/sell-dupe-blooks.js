d9d7e4d0e4758e06c90fa7fefc853b5253015628
soft-shelled-turtle <122046809+soft-shelled-turtle@users.noreply.github.com>
2023-02-07 18:01:03 +0000

    .

--- default-scripts/sell-dupe-blooks.js
+++ default-scripts/sell-dupe-blooks.js
@@ -1,46 +1,20 @@
-async function getName(authToken) {
-    const response = await fetch('https://api.blooket.com/api/users/verify-token?token=JWT+' + authToken);
-    const data = await response.json();
-
-    return data.name
-};
-
-async function getBlooks(blooketName) {
-    const response = await fetch('https://api.blooket.com/api/users/blooks?name=' + blooketName, {
-        headers: {
-            "referer": "https://www.blooket.com/",
-            "content-type": "application/json",
-            "authorization": localStorage.token
-        }
-    });
-    const data = await response.json();
-
-    return data
-};
-
-async function sellDupeBlooks() {
-    const myToken = localStorage.token.split('JWT ')[1];
-    const blooketName = await getName(myToken);
-    const blooks = await getBlooks(blooketName);
-    const x = Object.entries(blooks).filter(i => i[1] > 1).map(i => [i[0], i[1] - 1])
-
-    for (const [blook, val] of x) {
-        await fetch('https://api.blooket.com/api/users/sellblook', {
-            method: "PUT",
-            headers: {
-                "referer": "https://www.blooket.com/",
-                "content-type": "application/json",
-                "authorization": localStorage.token
-            },
-            body: JSON.stringify({
-                blook: blook,
-                name: blooketName,
-                numSold: val
-            })
-        });
-    };
-
-    alert('Results:\n' + x.map(x => `    ${x[1]} ${x[0]}`).join('\n'))
-};
-
-sellDupeBlooks();
+let iframe = document.createElement('iframe');
+document.body.append(iframe);
+window.alert = iframe.contentWindow.alert.bind(window);
+window.confirm = iframe.contentWindow.confirm.bind(window);
+iframe.remove();
+var axios = Object.values(webpackJsonp.push([
+    [], {
+        ['']: (_, a, b) => {
+            a.cache = b.c
+        },
+    },
+    [
+        ['']
+    ],
+]).cache).find((x) => x.exports?.a?.get).exports.a;
+axios.get("/api/users").then(async ({ data: { name, unlocks } }) => {
+    let blooks = Object.entries(unlocks).filter(x => x[1] > 1);
+      for (var [blook, amount] of blooks) await axios.put("/api/users/sellblook", { name, blook, numSold: amount - 1 });
+      alert(`Sold Blooks: \n${blooks.map(([blook, amount]) => `- ${blook} ${amount - 1}`).join(`\n`)}`);
+});
