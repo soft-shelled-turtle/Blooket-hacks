c22a54c471d4cca7488aae881c511f384b7819cc
soft-shelled-turtle <122046809+soft-shelled-turtle@users.noreply.github.com>
2023-02-08 22:03:38 +0000

    .

--- fun-stuff-that-isn't-for-Blooket/Popup-cat-image.js
+++ fun-stuff-that-isn't-for-Blooket/Popup-cat-image.js
@@ -1,2 +1,2 @@
-var image = document.getElementById("myImage").src="cat.gif";
+var image = document.getElementById("myImage").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTN-TnAjKR-raPt-5fkObEAN04ktV6GB_1OvmkvxzZH&s";
 alert("OnLoad image"+image );
