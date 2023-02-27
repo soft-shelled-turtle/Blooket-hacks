e6a1a57576b24fdfb989df0735f6886146f24495
soft-shelled-turtle <122046809+soft-shelled-turtle@users.noreply.github.com>
2023-02-27 20:35:08 +0000

    .

--- crazy-kindom/max-stats.js
+++ crazy-kindom/max-stats.js
@@ -12,7 +12,7 @@
             if (window.location.pathname != '/kingdom') {
                 alert('You must be in a crazy kingdom game!');
             } else {
-                reactHandler().stateNode.setState({ materials: 100, people: 100, happiness: 100, gold: 100 });
+                reactHandler().stateNode.setState({ materials: 999, people: 999 , happiness: 999, gold: 999 });
         
                 alert('It rained max stats. Have fun!');
             };
